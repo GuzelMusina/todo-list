de699bbce4bebd22479b401825cbe06eb088808b
GuzelMusina <guzmus@rambler.ru>
2019-09-26 19:47:48 +0300

    implemented input layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import './App.css';
+import './App.module.css';
+import styles from './App.module.css';
+import {Button, Input} from "antd";
 
 function App() {
-  return (
-    <div>
-      todolist app
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>
+                {/*контейнер для двух элементов, пропс primary делает главной кнопку на сайте*/}
+                <div className={styles.inputContainer}>
+                    <Input/>
+                    <Button type="primary" className={styles.actionButton}>+ Add</Button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
