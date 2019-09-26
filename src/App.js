b0673a05c4617c002a90332ba98fd2cd3af294f1
GuzelMusina <guzmus@rambler.ru>
2019-09-26 20:13:09 +0300

    implemented todo items layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.module.css';
 import styles from './App.module.css';
-import {Button, Input} from "antd";
+import {Button, Checkbox, Input} from "antd";
 
 function App() {
+    const todos = [1,2,3,4,5,6,7,8,9,10];
+
     return (
         <div className={styles.container}>
             <div className={styles.content}>
@@ -12,9 +14,25 @@ function App() {
                     <Input/>
                     <Button type="primary" className={styles.actionButton}>+ Add</Button>
                 </div>
+
+                {/*сделать цикл и вывести 10 элементов*/}
+                {todos.map(()=> <TodoItem/>)}
+
+                <TodoItem/>
             </div>
         </div>
     );
+}
+
+function TodoItem() {
+    return (
+        <div className={styles.listContainer}>
+            <div className={styles.listItem}>
+                <Checkbox/>
+                <h2 className={styles.todoTitle}>buy fruits</h2>
+            </div>
+        </div>
+    )
 }
 
 export default App;
